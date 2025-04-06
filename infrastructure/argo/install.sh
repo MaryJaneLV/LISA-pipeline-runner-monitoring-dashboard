9a856514bda2454c9ed5209e06d03459d7f6de17
Manuel Salgado <masalgado@tripadvisor.com>
2025-04-06 22:39:33 +0100

    wip

--- infrastructure/argo/install.sh
+++ infrastructure/argo/install.sh
@@ -3,8 +3,6 @@ set -e
 
 echo "Installing Argo Workflows..."
 
-# Apply the modified manifest
-echo "Applying Argo Workflows manifest..."
 kubectl apply -f infrastructure/argo/install.yaml
 kubectl patch svc argo-server -n scientific-workflow --patch "$(cat ./infrastructure/argo/server-patch.yaml)"
 nohup kubectl -n scientific-workflow port-forward svc/argo-server 2746:2746 &
