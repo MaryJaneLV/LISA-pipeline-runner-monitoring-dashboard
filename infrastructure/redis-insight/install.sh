9512dfad7310694fe3c620b585b16c32cccaff2d
Manuel Salgado <masalgado@tripadvisor.com>
2025-04-06 21:57:23 +0100

    remove redis from infra

--- infrastructure/redis-insight/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing Redis Insight..."
-
-kubectl apply -f ./infrastructure/redis-insight/deployment.yaml
-
-echo "Redis insight installation completed!"
--- infrastructure/redis/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing Redis..."
-
-kubectl apply -f ./infrastructure/redis/deployment.yaml
-
-echo "Redis installation completed!"
