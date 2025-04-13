51baf8060a3a9f7a4c54a6604f9a623d599a9b28
Manuel Salgado <masalgado@tripadvisor.com>
2025-04-13 18:28:19 +0100

    delete example template

--- workflows-examples/data-pipeline/inputs/scripts/normalize/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Install required packages
-if [ -f /tmp/scripts/requirements.txt ]; then
-  pip install -r /tmp/scripts/requirements.txt
-fi
-
-python /tmp/scripts/script.py
