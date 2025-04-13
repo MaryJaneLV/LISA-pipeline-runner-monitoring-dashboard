0777de84200f3e80d94a092c013db4b5df703453
Manuel Salgado <masalgado@tripadvisor.com>
2025-04-13 16:33:44 +0100

    refactor workflow examples

---
+++ workflows-examples/number-processing/inputs/scripts/process/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Install required packages
+if [ -f /tmp/scripts/requirements.txt ]; then
+  pip install -r /tmp/scripts/requirements.txt
+else
+  pip install numpy==1.24.3
+  pip install pandas==1.5.3
+fi
+
+export OPERATION="$1"
+export FACTOR="$2"
+
+python /tmp/scripts/script.py
